d42fc33b59c62bbd4b197b552155f9cfe831645f
kstasiowska1 <kstasiowska1@babson.edu>
2026-02-03 11:16:01 -0500

    Adjusting recursive.py code.

--- code/recursive.py
+++ code/recursive.py
@@ -1,7 +1,10 @@
+#creating a recursive function that calls itself indefinitely
 def groundhog_day():
+    """A function that prints 'groundhog' day indefinitely."""""
     import time
     print('Did you mean: groundhog day?')
+
     time.sleep(1)
     groundhog_day()
 
-groundhog_day()
+groundhog_day
