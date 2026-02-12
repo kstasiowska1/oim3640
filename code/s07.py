19f50c632cbba38fe5668fa8de54379d7372bb2d
kstasiowska1 <kstasiowska1@babson.edu>
2026-02-12 10:41:09 -0500

    Adjusting session 7 and 8 code.

--- code/s07.py
+++ code/s07.py
@@ -1,4 +1,32 @@
 import turtle
+
+def draw_square(turtle_obj, size = 100):
+    '''Draw a square with a given size
+    '''
+    for _ in range(4):
+        turtle_obj.forward(size)
+        turtle_obj.left(90)
+
+def draw_spiral(t):
+    '''
+    Draw one square, turn an angle, then draw another square and so on
+    '''
+
+    for i in range(16):
+        draw_square(t, 50 + i * 2)
+        t.left(10)
+
+def main():
+    t = turtle.Turtle()
+    t.speed(0)
+    draw_spiral(t)
+    turtle.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+
+'''
 t = turtle.Turtle()
 t.speed(0)
 
@@ -11,3 +39,4 @@ def main():
     for i in range (36):
         draw_square()
         t.left(10)
+'''
