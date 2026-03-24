2f84843adc2ee14dfd5c2b690941b3b758fceb3c
kstasiowska1 <kstasiowska1@babson.edu>
2026-03-23 21:02:57 -0400

    Creating a function which cleans the text of punctuation and upper case.

--- mini project 2/mini_proj_2_code.py
+++ mini project 2/mini_proj_2_code.py
@@ -6,4 +6,19 @@ def load_text(file_name):
     """
     with open(file_name, "r", encoding="utf-8") as file:
         return file.read()
-    
+
+def clean_and_split_text(text):
+    """
+    Converts the text to lowercase and removes common punctuation.
+    Then it splits the text into a list of individual words.
+    """
+    text = text.lower()
+    text = text.replace(".", "")
+    text = text.replace(",", "")
+    text = text.replace("!", "")
+    text = text.replace("?", "")
+    text = text.replace("-", "")
+    text = text.replace(":", "")
+    text = text.replace(";", "")
+    words = text.split()
+    return words
