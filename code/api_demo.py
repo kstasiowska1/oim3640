dcc6fbfcfc093ec815ecb825b32b8550cb1c3091
kstasiowska1 <kstasiowska1@babson.edu>
2026-03-31 11:12:10 -0400

    Session 18 in class demo on APIs.

---
+++ code/api_demo.py
@@ -0,0 +1,40 @@
+import requests
+
+response = requests.get(
+    'https://oim.108122.xyz/words/random',
+    headers = {'X-Token': 'kamilakamila'})
+print(response.json())
+
+response = requests.get('https://oim.108122.xyz/mass')
+data = response.json()
+
+#print(data['name'])       # 'Massachusetts'
+#print(data['governor'])   # 'Maura Healey'
+
+for town in data['data'][:5]:
+    print(f"{town['name']}: pop {town['population']:,}")
+
+print(len(data))
+print(data.keys)
+print(type(data['data'])) # do this to explore data structure
+
+# seeing how many towns there are in the data
+towns = data['data']
+print(type(towns)) # list
+print(len(towns)) # 351  
+
+# find smallest town
+smallest = min(towns, key=lambda t: t['population'])
+print(f"Smallest town: {smallest['name']} with population {smallest['population']:,}")
+
+# find top 5 largest towns
+largest = sorted(towns, key=lambda t: t['population'], reverse=True)[:5]
+print("Top 5 largest towns:")
+for town in largest:
+    print(f"{town['name']}: pop {town['population']:,}")
+
+# find top 5 smallest towns
+smallest_5 = sorted(towns, key=lambda t: t['population'])[:5]
+print("Top 5 smallest towns:")
+for town in smallest_5:
+    print(f"{town['name']}: pop {town['population']:,}")
