195e9a45a963b6383bedf1b4997ede66c9d1d454
kstasiowska1 <kstasiowska1@babson.edu>
2026-04-15 17:37:04 -0400

    Adding a map with a red pin for the location that the user searches for.

--- mini project 3/app.py
+++ mini project 3/app.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
 from mbta_helper import get_coordinates, find_nearest_station
 
+load_dotenv()
+
 app = Flask(__name__)
+MAPBOX_API_KEY = os.getenv("MAPBOX_API_KEY")
 
 @app.route('/')
 def home():
@@ -17,12 +22,24 @@ def search():
         return render_template(
             "results.html",
             place=place,
-            station="Location not found"
+            station="Location not found",
+            map_urp=None
         )
 
     station = find_nearest_station(latitude, longitude)
 
-    return render_template("results.html", place=place, station=station)
+    map_url = (
+    f"https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/"
+    f"pin-s+ff0000({longitude},{latitude})/"
+    f"{longitude},{latitude},14/600x400?access_token={MAPBOX_API_KEY}"
+)
+
+    return render_template(
+        "results.html", 
+        place=place, 
+        station=station,
+        map_url=map_url
+        )
 
 if __name__ == "__main__":
     app.run(debug=True)
